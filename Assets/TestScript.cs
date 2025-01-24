50b93776c5bd65eede470cc86f3dcad1dc23220c
zachlindblad <zachlindblad@gmail.com>
2025-01-23 17:52:52 -0800

    I made an edit to this file

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,7 +5,9 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int i = 0;
+        i++;
+        //I changed something
     }
 
     // Update is called once per frame
