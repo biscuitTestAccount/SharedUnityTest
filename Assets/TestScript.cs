1172077194fd6f9c7af248d59e86be055853326e
zachlindblad <zachlindblad@gmail.com>
2025-01-23 18:19:13 -0800

    hi there

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,6 +5,7 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // hi there
         int i = 0;
         i++;
         //I changed something
