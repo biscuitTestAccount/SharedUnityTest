d5f9b5b63010d2d0747adf738b4dd183a1c0d419
zachlindblad <zachlindblad@gmail.com>
2025-02-02 14:56:03 -0800

    New commit in test branch

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -13,10 +13,7 @@ public class TestScript : MonoBehaviour
         int iter = 0;
         iter++;
         Debug.Log(iter);
-        //I changed something
-        //and this
-        //and this-chanee
-        //Hi there again
+
         while (iter<10)
         {
             iter++;
